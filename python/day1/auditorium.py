978fd62c054d46f930d4e3f205421ea521201ca6
VIYYRS <saltlogs.1724@gamil.com>
2023-11-20 16:11:51 +0900

    first commit

---
+++ python/day1/auditorium.py
@@ -0,0 +1,18 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with '+' sign.")
+print("e.g. print('Hello ' + 'World')")
+print("New lines can be created with a backslash and n.")
+
+print(len(input('enter anything')))
+
+a = input('enter a value')
+b = input('enter b value')
+c = a
+a = b
+b = c
+print('a: ' + a)
+print('b: ' + b)
---
+++ python/day1/main.py
@@ -0,0 +1,16 @@
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+print('Hello ' + input('What is your name?') + '!')
+
+name = input('What is your name?')
+print(name)
+
+name = 'Jack'
+print(name)
+
+name = 'Saltlogs'
+print(name)
+
+user_name = input('What is your name?')
+length = len(name)
+print(length)
